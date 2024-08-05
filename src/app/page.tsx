5d805cfd0ab224856c841cd682c9a76b31d2435c
Cheng Zheng <2841102z@student.gla.ac.uk>
2024-08-05 13:31:21 +0100

    deploy test

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,33 +1,33 @@
-import Image from "next/image";
+
 import React from 'react';
 import Link from 'next/link';
 
 
 export default function Home() {
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">      
-      <div> test</div>
-      <div>
-            <h1>Welcome to the Next.js App</h1>
-            <Link href="/2-game" legacyBehavior>
-                <a style={{ textDecoration: 'none' }}>
-                    <button style={{ padding: '10px 20px', fontSize: '16px', cursor: 'pointer' }}>
-                        Go to 2-player game
-                    </button>
-                </a>
-            </Link>
-        </div>
-        <div>
-            <h1>Welcome to the Next.js App</h1>
-            <Link href="/test" legacyBehavior>
-                <a style={{ textDecoration: 'none' }}>
-                    <button style={{ padding: '10px 20px', fontSize: '16px', cursor: 'pointer' }}>
-                        Go to 2-player game
-                    </button>
-                </a>
-            </Link>
-        </div>
-    </main>
-  );
+    return (
+        <main className="flex min-h-screen flex-col items-center justify-between p-24">
+            <div> test</div>
+            <div>
+                <h1>Welcome to the Next.js App</h1>
+                <Link href="/2-game" legacyBehavior>
+                    <a style={{ textDecoration: 'none' }}>
+                        <button style={{ padding: '10px 20px', fontSize: '16px', cursor: 'pointer' }}>
+                            Go to 2-player game
+                        </button>
+                    </a>
+                </Link>
+            </div>
+            <div>
+                <h1>Welcome to the Next.js App</h1>
+                <Link href="/test" legacyBehavior>
+                    <a style={{ textDecoration: 'none' }}>
+                        <button style={{ padding: '10px 20px', fontSize: '16px', cursor: 'pointer' }}>
+                            Go to 2-player game
+                        </button>
+                    </a>
+                </Link>
+            </div>
+        </main>
+    );
 
 }
